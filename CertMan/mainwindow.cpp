49c51e3e7e7d251dd7ecad634f298db0ebcb1927
jykim <jykim@ranix.co.kr>
2023-06-12 16:33:14 +0900

    KeyPair 목록에서 기본 좌우 스크롤 되지 않게 변경

--- CertMan/mainwindow.cpp
+++ CertMan/mainwindow.cpp
@@ -3279,7 +3279,7 @@ void MainWindow::createRightKeyPairList()
     right_table_->setColumnWidth( 2, 80 );
     right_table_->setColumnWidth( 3, 300 );
     right_table_->setColumnWidth( 4, 80 );
-    right_table_->setColumnWidth( 5, 60 );
+    right_table_->setColumnWidth( 5, 40 );
 
     for( int i = 0; i < keyPairList.size(); i++ )
     {
