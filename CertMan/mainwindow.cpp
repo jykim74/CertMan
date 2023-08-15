78c971c0e9952d0b076408923fdd0395f048b117
jykim <jykim74@gmail.com>
2023-08-15 21:14:15 +0900

    Remote DB 라이트모드에서 지원

--- CertMan/mainwindow.cpp
+++ CertMan/mainwindow.cpp
@@ -235,15 +235,12 @@ void MainWindow::createActions()
     fileMenu->addAction(openAct);
     fileToolBar->addAction(openAct);
 
-    if( manApplet->isLicense() )
-    {
-        const QIcon remotedbIcon = QIcon::fromTheme("document-remotedb", QIcon(":/images/remotedb.png"));
-        QAction *remoteDBAct = new QAction( remotedbIcon, tr("&Remote Database"), this );
-        remoteDBAct->setStatusTip(tr("Connect Remote Database"));
-        connect( remoteDBAct, &QAction::triggered, this, &MainWindow::remoteDB);
-        fileMenu->addAction(remoteDBAct);
-        fileToolBar->addAction(remoteDBAct);
-    }
+    const QIcon remotedbIcon = QIcon::fromTheme("document-remotedb", QIcon(":/images/remotedb.png"));
+    QAction *remoteDBAct = new QAction( remotedbIcon, tr("&Remote Database"), this );
+    remoteDBAct->setStatusTip(tr("Connect Remote Database"));
+    connect( remoteDBAct, &QAction::triggered, this, &MainWindow::remoteDB);
+    fileMenu->addAction(remoteDBAct);
+    fileToolBar->addAction(remoteDBAct);
 
     const QIcon logoutIcon = QIcon::fromTheme("document-logout", QIcon(":/images/logout.png"));
     QAction *logoutAct = new QAction( logoutIcon, tr("&Logout"), this );
