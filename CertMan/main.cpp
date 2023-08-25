aca5ea97a20a4e062cbf1155ed30a66d1e8b59ad
jykim <jykim@ranix.co.kr>
2023-08-25 16:24:11 +0900

    OS 별 qss 지원

--- CertMan/main.cpp
+++ CertMan/main.cpp
@@ -6,6 +6,30 @@
 #include "i18n_helper.h"
 #include "settings_mgr.h"
 
+void setQss( QApplication* app )
+{
+    QString strStyle;
+    QFile qss(":/certman.qss");
+    qss.open( QFile::ReadOnly );
+
+    strStyle = qss.readAll();
+
+#if defined( Q_OS_WIN32 )
+    QFile css( ":/qt-win.css" );
+#elif defined( Q_OS_MAC)
+    QFile css( ":/qt-mac.css" );
+#endif
+    css.open( QFile::ReadOnly );
+
+    if( css.size() > 0 )
+    {
+        strStyle += "\n";
+        strStyle += css.readAll();
+    }
+
+    app->setStyleSheet( strStyle );
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -14,9 +38,13 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain( "jssoft.com" );
     QCoreApplication::setApplicationName( "CertMan" );
 
+#if 0
     QFile qss(":/certman.qss");
     qss.open( QFile::ReadOnly );
     app.setStyleSheet(qss.readAll());
+#else
+    setQss( &app );
+#endif
 
     QCommandLineParser parser;
     parser.setApplicationDescription( QCoreApplication::applicationName() );
