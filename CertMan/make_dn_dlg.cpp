e3251e7975f2e4aaa4f9a3fd54dcb1de87314eb5
jykim <jykim74@gmail.com>
2023-10-23 21:47:36 +0900

    MakeDN works

--- CertMan/make_dn_dlg.cpp
+++ CertMan/make_dn_dlg.cpp
@@ -1,4 +1,9 @@
+#include <QStringList>
+
 #include "make_dn_dlg.h"
+#include "mainwindow.h"
+#include "man_applet.h"
+#include "settings_mgr.h"
 
 MakeDNDlg::MakeDNDlg(QWidget *parent) :
     QDialog(parent)
@@ -7,6 +12,31 @@ MakeDNDlg::MakeDNDlg(QWidget *parent) :
 }
 
 MakeDNDlg::~MakeDNDlg()
+{
+
+}
+
+void MakeDNDlg::setDN( const QString strDN )
+{
+    QStringList strList = strDN.split( "," );
+
+    for( int i = 0; i < strList.size(); i++ )
+    {
+        QString strPart = strList.at(i);
+        QStringList strNVList = strPart.split( "=" );
+        if( strNVList.size() != 2 ) continue;
+
+        QString strName = strNVList.at(0);
+        QString strValue = strNVList.at(1);
+
+        if( strName == "CN" )
+        {
+            mCNText->setText( strValue );
+        }
+    }
+}
+
+const QString MakeDNDlg::getDN()
 {
 
 }
--- CertMan/make_dn_dlg.h
+++ CertMan/make_dn_dlg.h
@@ -2,19 +2,21 @@
 #define MAKE_DN_DLG_H
 
 #include <QDialog>
-#include "ui_make_cert_dlg.h"
+#include "ui_make_dn_dlg.h"
 
 namespace Ui {
 class MakeDNDlg;
 }
 
-class MakeDNDlg : public QDialog, public Ui::MakeCertDlg
+class MakeDNDlg : public QDialog, public Ui::MakeDNDlg
 {
     Q_OBJECT
 
 public:
     explicit MakeDNDlg(QWidget *parent = nullptr);
     ~MakeDNDlg();
+    void setDN( const QString strDN );
+    const QString getDN();
 
 private:
 
