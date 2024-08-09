553d7d6fd76fb0f460d56e76fca7118337248d72
jykim <jykim@ranix.co.kr>
2024-08-09 11:14:07 +0900

    라이선스 Update 버튼은 유효한 경우만 표시

--- CertMan/lcn_info_dlg.cpp
+++ CertMan/lcn_info_dlg.cpp
@@ -69,8 +69,7 @@ QString LCNInfoDlg::getLicenseURI()
 void LCNInfoDlg::initialize()
 {
     int ret = 0;
-    mUpdateBtn->setEnabled( false );
-
+    mUpdateBtn->hide();
 
     JS_LICENSE_INFO sLicenseInfo = manApplet->LicenseInfo();
     QString strEmail = manApplet->settingsMgr()->getEmail();
@@ -100,8 +99,7 @@ void LCNInfoDlg::initialize()
         if( ret == JSR_VALID )
         {
             mCurGroup->setEnabled( true );
-            mUpdateBtn->setEnabled( true );
-
+            mUpdateBtn->show();
             mRemoveBtn->hide();
         }
         else
