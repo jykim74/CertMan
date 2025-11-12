acff41bb179963d6ba9382ba2fe8eee12fd87d00
jykim74 <jykim74@gmail.com>
2025-11-12 22:40:25 +0900

    Version 2.2.6A

--- CertMan/mainwindow.cpp
+++ CertMan/mainwindow.cpp
@@ -4454,6 +4454,8 @@ void MainWindow::expandItem( ManTreeItem *item )
             pSubCAItem->setIcon(QIcon(":/images/ca.png"));
             pSubCAItem->setDataNum( certRec.getNum() );
             pCAItem->appendRow( pSubCAItem );
+
+            expandItem( pSubCAItem );
         }
 
 //        left_tree_->expand( pCAItem->index() );
